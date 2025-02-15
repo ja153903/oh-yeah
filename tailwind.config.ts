1ca31d26341e252600d8438a99a60ed5c11b8a3d
ja153903 <jaimeabbariao@gmail.com>
2025-02-15 16:07:06 -0500

    setting up tailwind tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,35 @@
+import tailwindcssTypography from "@tailwindcss/typography"
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./mdx-components.tsx"
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        base03: '#002b36',
+        base02: '#073642',
+        base01: '#586e75',
+        base00: '#657b83',
+        base0: '#839496',
+        base1: '#93a1a1',
+        base2: '#eee8d5',
+        base3: '#002b36',
+        yellow: '#b58900',
+        orange: '#cb4b16',
+        red: '#dc322f',
+        magenta: '#d33682',
+        violet: '#6c71c4',
+        blue: '#268bd2',
+        cyan: '#2aa198',
+        green: '#859900',
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssTypography()],
 } satisfies Config;
