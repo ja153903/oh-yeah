2216eaa8168614396b90facfc6533531e26785be
ja153903 <jaimeabbariao@gmail.com>
2025-02-15 16:07:48 -0500

    adding mdx support to config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import createMDX from "@next/mdx"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true
+  }
 };
 
-export default nextConfig;
+const withMDX = createMDX()
+
+export default withMDX(nextConfig);
