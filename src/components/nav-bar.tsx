ea818b5cc65055752bd05f7f9adfbd4c03ac65e4
ja153903 <jaimeabbariao@gmail.com>
2025-02-15 23:49:15 -0500

    updating structure

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -5,8 +5,11 @@ import Link from 'next/link'
 export function NavBar() {
   return (
     <div className="sticky top-0 z-10 flex flex-col pt-10 pb-8 justify-center items-center w-full backdrop-filter backdrop-blur-lg px-4 gap-4">
-      <Link className="text-cyan font-geist-sans font-bold mr-3" href="/">
-        <span className="underline text-3xl">Oh yeah</span>{' '}
+      <Link
+        className="text-cyan font-geist-sans font-bold mr-3 underline text-3xl"
+        href="/"
+      >
+        Oh yeah
       </Link>
       <p className="text-md text-base00 dark:text-base0">
         a blog by <span className="text-green italic">Jaime Abbariao</span>
