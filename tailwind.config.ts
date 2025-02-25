648d753843d1bacdc15fd36cf1cc760e9ef1e17a
ja153903 <jaimeabbariao@gmail.com>
2025-02-24 23:20:47 -0500

    removing content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ import type { Config } from 'tailwindcss'
 export default {
   darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './mdx-components.tsx',
